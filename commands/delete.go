3729f524f78346997bf262a962a5ee5a2d534db2
Bennett Wetters <contact@bendo.org>
2021-11-10 17:43:07 +0100

    Add delete command

--- commands/delete.go
+++ commands/delete.go
@@ -1,6 +1,9 @@
 package commands
 
 import (
+	"log"
+
+	"RoBOT/config"
 	"RoBOT/helptexts"
 	"RoBOT/util"
 
@@ -8,8 +11,47 @@ import (
 )
 
 func deleteRun(s *discordgo.Session, ev *discordgo.MessageCreate, args []string) {
-	_, err := s.ChannelMessageSend(ev.ChannelID, "Pong!")
-	util.CheckMsgSend(err, ev.GuildID, ev.ChannelID)
+	// ErrCheck if user has permission
+	userPerms, err := s.State.MessagePermissions(ev.Message)
+	util.ErrCheck(err, "[Delete] Failed getting permissions for user "+ev.Author.Mention())
+	if userPerms&discordgo.PermissionManageChannels != discordgo.PermissionManageChannels {
+		log.Printf("[Delete] User %s has no permissions to delete channel!", ev.Author.Mention())
+		// TODO Send message to notify user
+		return
+	}
+
+	// Get channel
+	channel, err := s.Channel(ev.ChannelID)
+	util.ErrCheck(err, "[Archive] Failed getting channel for ID "+ev.ChannelID)
+
+	// Don't continue if we are inside the archive
+	if channel.ParentID == config.ServerConfig.ArchiveCategoryID {
+		log.Printf(
+			"[Delete] User %s tried deleting channel %s but it's in the archive!", ev.Author.Mention(), channel.Mention(),
+		)
+		return
+	}
+
+	log.Printf("[Delete] User %s invoked DELETE in channel %s", ev.Author.Mention(), channel.Mention())
+
+	// Get channels in category (catChs)
+	var catChs []*string
+	gChs, err := s.GuildChannels(ev.GuildID)
+	util.ErrCheck(err, "[Archive] Failed getting guild channels")
+	for _, gch := range gChs {
+		if gch.ParentID == channel.ParentID && gch.ID != channel.ID {
+			catChs = append(catChs, &gch.ID)
+		}
+	}
+	// Also append parent category to remove later
+	catChs = append(catChs, &channel.ParentID)
+
+	// Delete all channels and the category
+	for _, ch := range catChs {
+		_, err = s.ChannelDelete(*ch)
+		util.ErrCheck(err, "[Delete] Failed deleting channel "+*ch)
+		log.Printf("[Delete] User %s deleted channel %d", ev.Author.Mention(), ch)
+	}
 }
 
 func deleteHelp(s *discordgo.Session, ev *discordgo.MessageCreate, args []string) {
