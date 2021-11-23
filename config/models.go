6d979f787d5a060f35c1f9d730bf130b331bb86c
Bennett Wetters <contact@bendo.org>
2021-11-23 19:32:14 +0100

    Document ServerConf struct

--- config/models.go
+++ config/models.go
@@ -16,18 +16,21 @@ type TeamConf struct {
 
 // ServerConf represents db/server.json
 type ServerConf struct {
-	EventName               string                 `json:"EventName"`
-	GuildID                 string                 `json:"GuildID"`
+	EventName string `json:"EventName"`
+	GuildID   string `json:"GuildID"`
+	// Channels
 	VoiceChannelCreateID    string                 `json:"VoiceChannelCreateID"`
 	ArchiveCategoryID       string                 `json:"ArchiveCategoryID"`
 	WelcomeChannelID        string                 `json:"WelcomeChannelID"`
 	RoleAssignmentChannelID string                 `json:"RoleAssignmentChannelID"`
-	EveryoneRoleID          string                 `json:"EveryoneRoleID"`
-	ParticipantRoleID       string                 `json:"ParticipantRoleID"`
-	OrgaTeamRoleID          string                 `json:"OrgaTeamRoleID"`
-	BotAdminRoleID          string                 `json:"BotAdminRoleID"`
 	ProtectedChannels       map[string]interface{} `json:"ProtectedChannels"`
-	PermissionTemplates     struct {
+	// Roles
+	EveryoneRoleID    string `json:"EveryoneRoleID"`
+	ParticipantRoleID string `json:"ParticipantRoleID"`
+	OrgaTeamRoleID    string `json:"OrgaTeamRoleID"`
+	BotAdminRoleID    string `json:"BotAdminRoleID"`
+	// Permissions
+	PermissionTemplates struct {
 		Everyone    int64 `json:"Everyone"`
 		OrgaTeam    int64 `json:"Orga-Team"`
 		Participant int64 `json:"Participant"`
