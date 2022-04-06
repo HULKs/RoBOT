e13b1f79ea249cce0914493a915b14f9ae174eb4
Bennett Wetters <contact@bendo.org>
2022-04-06 20:16:42 +0200

    Add team and setup bootstrap /-commands

--- commands/commands.go
+++ commands/commands.go
@@ -1,6 +1,8 @@
 package commands
 
 import (
+	"RoBOT/config"
+
 	dg "github.com/bwmarrin/discordgo"
 )
 
@@ -18,6 +20,31 @@ var Commands = []*dg.ApplicationCommand{
 		Name:        "ping",
 		Description: "If online, the bot answers with: Pong!",
 	},
+	{
+		Name:        "team",
+		Description: "Assign yourself to a team",
+		Options: []*dg.ApplicationCommandOption{
+			{
+				Type:        dg.ApplicationCommandOptionInteger,
+				Name:        "team-number",
+				Description: "Number of the team you want to assign yourself to",
+				MinValue:    new(float64),
+				MaxValue:    float64(len(config.TeamList)),
+				Required:    true,
+			},
+		},
+	},
+	{
+		Name:        "setup",
+		Description: "Manage the server",
+		Options: []*dg.ApplicationCommandOption{
+			{
+				Name:        "bootstrap",
+				Description: "!DESTRUCTIVE! Bootstrap the server from scratch",
+				Type:        dg.ApplicationCommandOptionSubCommand,
+			},
+		},
+	},
 }
 
 var CommandHandlers = map[string]func(s *dg.Session, i *dg.InteractionCreate){
@@ -30,5 +57,32 @@ var CommandHandlers = map[string]func(s *dg.Session, i *dg.InteractionCreate){
 				},
 			},
 		)
+	},
+	"team": func(s *dg.Session, i *dg.InteractionCreate) {
+		team := config.TeamList[i.ApplicationCommandData().Options[0].IntValue()]
+		assignMemberToTeam(s, i.Member, team, i.GuildID)
+		s.InteractionRespond(
+			i.Interaction, &dg.InteractionResponse{
+				Type: dg.InteractionResponseChannelMessageWithSource,
+				Data: &dg.InteractionResponseData{
+					Content: "Assigned you to team " + team.Name,
+				},
+			},
+		)
+	},
+	"setup": func(s *dg.Session, i *dg.InteractionCreate) {
+		switch i.ApplicationCommandData().Options[0].Name {
+		case "bootstrap":
+			s.InteractionRespond(
+				i.Interaction, &dg.InteractionResponse{
+					Type: dg.InteractionResponseChannelMessageWithSource,
+					Data: &dg.InteractionResponseData{
+						Content: "Bootstrapping server...",
+					},
+				},
+			)
+			setupBootstrap(s, i.GuildID, i)
+		default:
+		}
 	},
 }
